ea638f7eca0ca1cfb3f94892322f3d8d2fbc00e0
nicola gallaccio <nicolagallaccio240815@gmail.com>
2024-02-01 18:02:40 +0100

    header ultimato

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('ciao'); 
+// CATTURA ICONA NAVBAR
+let fingerIcon = document.querySelector('#fingerIcon');
+
+// VARIABILE D'APPOGGIO PER L'ICONA NAVBAR
+
+let confirm = false;
+
+fingerIcon.addEventListener('click', ()=>{
+
+    if (confirm == false) {
+
+        fingerIcon.style.transform = 'rotate(180deg)';
+        confirm = true;
+
+    } else {
+
+        fingerIcon.style.transform = 'rotate(0deg)';
+        confirm = false;
+    }
+    
+});
